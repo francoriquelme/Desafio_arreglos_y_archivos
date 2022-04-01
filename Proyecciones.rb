73c4dcf2df5594f8883db94d29d7dd42099b7d43
francoriquelme <francoestebanru@gmail.com>
2022-04-01 19:01:23 -0300

    Inicio

---
+++ Proyecciones.rb
@@ -0,0 +1,6 @@
+# Crear el programa proyecciones.rb y un archivo con las proyecciones de venta de cada
+# mes del próximo año. Este archivo debe llamarse ventas_base.db y estar en la misma
+# carpeta de trabajo que el código. Para realizar el ejercicio puedes agregar como contenido
+# los siguientes datos:
+# 300070,50520,35000,32810,47999,62050,89100,21000,100010,121900,99549,210
+# 000
